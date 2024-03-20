e02aa9dc756ef3bc69188d627a7dfa468847d808
Cody Milliron <codymilliron.dev@gmail.com>
2024-03-20 16:04:59 -0400

    Added greating. And filed path constant.

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+FILE_PATH = "files/todos.txt"
+
 def print_list(todo_list):
     # list_for_print = [item.strip('\n') for item in todo_list]
     for i, item in enumerate(todo_list):
@@ -6,12 +8,12 @@ def print_list(todo_list):
     print(f"You have {len(todo_list)} yet to do.")
 
 
-def get_todos_from_file(filepath="files/todos.txt"):
+def get_todos_from_file(filepath=FILE_PATH):
     with open(filepath, "r") as file:
         content = file.readlines()
     return content
 
 
-def save_todos_to_files(todo_list, filepath="files/todos.txt"):
+def save_todos_to_files(todo_list, filepath=FILE_PATH):
     with open(filepath, "w") as file:
         file.writelines(todo_list)
--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 from functions import print_list, \
     get_todos_from_file, \
     save_todos_to_files
+import time
 
 user_prompt: str = "Type add, show, edit, delete, complete, or exit: "
 # todos = ["Clean\n", "Cook\n", "Mow the lawn\n"]
-FILE_PATH = "files/todos.txt"
-
-
-
-
-
-
-
+now = time.strftime("%B %d, %Y %H:%M:%S")
 
 def show_list():
     todos_list = get_todos_from_file()
@@ -19,56 +13,57 @@ def show_list():
     return todos_list
 
 
-while True:
-    user_action = input(user_prompt).strip()
-    if user_action.startswith("add"):
-        todos = get_todos_from_file()
-        # todo = input("Enter a todo: ")
-        todo = user_action[4:]
-        todos.append(todo + "\n")
-        save_todos_to_files(todos)
-    elif user_action.startswith('show'):
-        show_list()
-    elif user_action.startswith('exit'):
-        break
-    elif user_action.startswith('edit'):
-        try:
+if __name__ == "__main__":
+    print(f"It is {now}.")
+    while True:
+        user_action = input(user_prompt).strip()
+        if user_action.startswith("add"):
             todos = get_todos_from_file()
-            # item_number = int(input("Number of the todo to edit: "))
-            item_number = int(user_action[5:])
-            item_index = item_number - 1
-
-            item_to_edit = todos[item_index]
-            print(f"Item to edit: {item_to_edit}")
-            todos[item_index] = input("Enter edited todo: ") + "\n"
+            # todo = input("Enter a todo: ")
+            todo = user_action[4:]
+            todos.append(todo + "\n")
             save_todos_to_files(todos)
-        except ValueError:
-            print('You must enter a item number.')
-            continue
-        except IndexError:
-            print("You must enter a valid number.")
-            continue
-    elif user_action.startswith('complete'):
-        try:
-            todos = get_todos_from_file()
-            # item_number = int(input("What task have you completed: "))
-            item_number = int(user_action[9:])
-            item_index = item_number - 1
-            completed_item = todos.pop(item_index).strip("\n")
-            print(f'You completed {completed_item}. Remaining items:')
-            print_list(todos)
-            save_todos_to_files(todos)
-        except ValueError:
-            print('You must enter a item number.')
-            continue
-        except IndexError:
-            print("You must enter a valid number.")
-            continue
-    else:
-        print("Hey, you entered an unknown command")
+        elif user_action.startswith('show'):
+            show_list()
+        elif user_action.startswith('exit'):
+            break
+        elif user_action.startswith('edit'):
+            try:
+                todos = get_todos_from_file()
+                # item_number = int(input("Number of the todo to edit: "))
+                item_number = int(user_action[5:])
+                item_index = item_number - 1
 
+                item_to_edit = todos[item_index]
+                print(f"Item to edit: {item_to_edit}")
+                todos[item_index] = input("Enter edited todo: ") + "\n"
+                save_todos_to_files(todos)
+            except ValueError:
+                print('You must enter a item number.')
+                continue
+            except IndexError:
+                print("You must enter a valid number.")
+                continue
+        elif user_action.startswith('complete'):
+            try:
+                todos = get_todos_from_file()
+                # item_number = int(input("What task have you completed: "))
+                item_number = int(user_action[9:])
+                item_index = item_number - 1
+                completed_item = todos.pop(item_index).strip("\n")
+                print(f'You completed {completed_item}. Remaining items:')
+                print_list(todos)
+                save_todos_to_files(todos)
+            except ValueError:
+                print('You must enter a item number.')
+                continue
+            except IndexError:
+                print("You must enter a valid number.")
+                continue
+        else:
+            print("Hey, you entered an unknown command")
 
-print("Bye!")
+    print("Bye!")
 
 
 
