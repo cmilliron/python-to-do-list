1584da3a54bc8d54eca887010067786795bda4f3
Cody Milliron <codymilliron.dev@gmail.com>
2024-03-22 23:09:10 -0400

    Add ability to save todos to file, add exit button, updated requirements.txt file. Day 17.

--- todo_gui.py
+++ todo_gui.py
@@ -1,13 +1,28 @@
 import functions
 import PySimpleGUI as sg
 
+# Elements for GUI
 label = sg.Text("Type in a to-do:")
-input_box = sg.InputText(default_text="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button('Add')
 
-window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button]])
-window.read()
+exit_button = sg.Button('Exit')
+
+window = sg.Window(title="My To-Do App",
+                   layout=[[label],
+                           [input_box, add_button],
+                           [exit_button]],
+                   font=("Helvetica", 20))
+while True:
+    event, input_value = window.read()
+    match event:
+        case "Add":
+            todos = functions.get_todos_from_file()
+            todos.append(input_value['todo'] + "\n")
+            functions.save_todos_to_files(todos)
+        case "Exit" | sg.WIN_CLOSED:
+            break
+
 
 window.close()
 
