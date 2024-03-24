c77d857140f895322b2dbb80c9f99851a5c8b19f
Cody Milliron <codymilliron.dev@gmail.com>
2024-03-23 21:34:44 -0400

    Change from if statements to try statements for error checking. Add theme. Add calendar. Day 18

--- todo_gui.py
+++ todo_gui.py
@@ -1,7 +1,11 @@
 import functions
 import PySimpleGUI as sg
+import time
+
+sg.theme('DarkPurple4')
 
 # Elements for GUI
+date_label = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do:")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button('Add')
@@ -17,15 +21,17 @@ complete_button = sg.Button('Complete')
 exit_button = sg.Button('Exit')
 
 window = sg.Window(title="My To-Do App",
-                   layout=[[label],
+                   layout=[[date_label],
+                           [label],
                            [input_box, add_button],
                            [list_box, edit_button, complete_button],
                            [exit_button]],
                    font=("Helvetica", 20))
 while True:
-    event, input_value = window.read()
-    print(event)
-    print(input_value)
+    event, input_value = window.read(timeout=1000)
+    window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+    # print(event)
+    # print(input_value)
 
     match event:
         case "Add":
@@ -35,7 +41,7 @@ while True:
             window['todo_list'].update(values=todos)
             window['todo'].update('')
         case "Edit":
-            if len(input_value["todo_list"]) > 0:
+            try:
                 todo_to_edit = input_value["todo_list"][0]
                 new_todo = input_value['todo'] + "\n"
                 todos = functions.get_todos_from_file()
@@ -44,21 +50,21 @@ while True:
                 functions.save_todos_to_files(todos)
                 window['todo_list'].update(values=todos)
                 window['todo'].update('')
-            else:
-                sg.Popup("You didn't pick anything to edit.")
+            except IndexError:
+                sg.Popup("You didn't a todo to complete.", font=("Helvetica", 20))
                 continue
         case "todo_list":
             window['todo'].update(value=input_value['todo_list'][0])
         case "Complete":
-            if len(input_value["todo_list"]) > 0:
+            try:
                 todo_to_complete = input_value["todo_list"][0]
                 todos = functions.get_todos_from_file()
                 todos.remove(todo_to_complete)
                 functions.save_todos_to_files(todos)
                 window['todo_list'].update(values=todos)
                 window['todo'].update('')
-            else:
-                sg.Popup("You didn't a todo to complete.")
+            except IndexError:
+                sg.Popup("You didn't a todo to complete.", font=("Helvetica", 20))
                 continue
         case "Exit" | sg.WIN_CLOSED:
             break
